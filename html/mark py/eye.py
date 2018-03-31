49eb8be08abbfdb4eb0104c9ef3c10147dcf44c7
Yuyang Huang <yuyangh@usc.edu>
2018-03-31 14:25:26 -0700

    supply json return

--- html/mark py/eye.py
+++ html/mark py/eye.py
@@ -29,6 +29,87 @@ html = '''
     </form>
 '''
 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/home', methods=['PUT', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        # if 'file' not in request.files:
+        #     flash('No file part')
+        #     return redirect(request.url)
+        file = request.files['image']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)  # use secure_filename function for safety
+            # update the file & clean the photo cache
+            cleanPhotoCache()
+            print(photoGallary)
+            print(request.form)
+            # revise the filename with order
+            filename = filename.rsplit('.', 1)[0].lower() + str(len(photoGallary)) + "." + \
+                       filename.rsplit('.', 1)[1].lower()
+            photoGallary.append(filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(request.url)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    '''
+    #first render the template, then send post
+    return redirect(url_for('upload_file'))
+    '''
+    cleanPhotoCache()
+    if request.method == 'POST':
+        print(request.form)
+        if (request.form['tracking'] == "true"):
+            print("get tracking request")
+            # return something to
+            upload_file()
+            return "Jude is very Smart!!!"
+        else:
+            # it is the request for detecting
+            print("get detecting request")
+        print(url_for('upload_file'))
+        upload_file()
+    return render_template('Launch.html')
+
+
+def situationAnalysis():
+    data = {"spam": "foo", "parrot": 42}
+    #   name, x-pos, y-pos, area , motion
+    situation=[["Lion",100,100.5,2500,"to the right"],
+               ["Tiger", 200, 100.5, 2500, "to the right"],
+               ["UFO", 50, 100.5, 2500, "to the left"]]
+    in_json = json.dumps(situation)  # Encode the data
+    return  in_json
+
+
+def cleanPhotoCache():
+    # get all files in the upload folder
+    photoGallary = os.listdir(app.config['UPLOAD_FOLDER'])
+    # if there are files not belong to the folder, ignore them
+    for files in photoGallary:
+        if not allowed_file(files):
+            photoGallary.remove(files)
+    # delete files if more than limit
+    while len(photoGallary) > UPPER_SIZE_photoGallary:
+        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], photoGallary[0]))
+        del photoGallary[0]
+
+
+if __name__ == "__main__":
+    app.run()
+
 '''
 # original method, used for storage
 @app.route('/', methods=['GET', 'POST'])
@@ -78,75 +159,3 @@ def download(filename):
             return send_from_directory('upload', filename, as_attachment=True)
             # return send_from_directory('upload', filename, as_attachment=True)#original line
 '''
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-@app.route('/home', methods=['PUT', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        # if 'file' not in request.files:
-        #     flash('No file part')
-        #     return redirect(request.url)
-        file = request.files['image']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)  # use secure_filename function for safety
-            # update the file & clean the photo cache
-            photoGallary.append(filename)
-            cleanPhotoCache()
-            print(photoGallary)
-            print(request.form)
-            filename = filename.rsplit('.', 1)[0].lower() + str(len(photoGallary)) + "." + \
-                       filename.rsplit('.', 1)[1].lower()
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(request.url)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    '''
-    #first render the template, then send post
-    return redirect(url_for('upload_file'))
-    '''
-    cleanPhotoCache()
-    if request.method == 'POST':
-        print(request.form)
-        if(request.form['tracking']=="true"):
-            print("get tracking request")
-            return "Jude is very Smart!!!"
-        else:
-            # it is the request for detecting
-            print("get detecting request")
-        print(url_for('upload_file'))
-        upload_file()
-    return render_template('Launch.html')
-
-
-# @app.route('/about',methods=['GET','POST'])
-# def display():
-#     redirect(url_for())
-
-def cleanPhotoCache():
-    # get all files in the upload folder
-    photoGallary = os.listdir(app.config['UPLOAD_FOLDER'])
-    # if there are files not belong to the folder, ignore them
-    for files in photoGallary:
-        if (files[len(files) - 3:] not in ALLOWED_EXTENSIONS):
-            photoGallary.remove(files)
-
-    while len(photoGallary) > UPPER_SIZE_photoGallary:
-        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], photoGallary[0]))
-        del photoGallary[0]
-
-
-if __name__ == "__main__":
-    app.run()
