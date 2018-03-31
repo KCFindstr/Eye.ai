d4d55fc9849e005e3e928777139aa6a0e7aaa87e
Yuyang Huang <yuyangh@usc.edu>
2018-03-30 23:44:18 -0700

    update global variable

--- eye.ai/eye.py
+++ eye.ai/eye.py
@@ -1,28 +1,37 @@
 import os
 from flask import Flask, request, redirect, url_for, flash,send_file, render_template, send_from_directory
-
-'''
 from werkzeug.utils import secure_filename
 import requests
-'''
 
 app = Flask(__name__)
-'''
+
+
+#allowed format
+ALLOWED_EXTENSIONS = set(['jpg','jpeg'])
+#UPLOADED_PHOTOS_DEST = '/path/to/the/uploads'
+#UPLOADS_DEFAULT_DEST = '/path/to/the/uploads'
 # introduction http://flask.pocoo.org/docs/0.12/quickstart/#static-files
 # url_for('static', filename='style.css')
-#UPLOAD_FOLDER is where we will store the uploaded files
-UPLOAD_FOLDER = '/Users/yuyang/Desktop/temp'
-#allowed format
-ALLOWED_EXTENSIONS = set(['mp3','wav'])
+
+#used to store pictures
+photoGallary=[]
+
+html = '''
+    <!DOCTYPE html>
+    <title>Upload File</title>
+    <h1>图片上传</h1>
+    <form method=post enctype=multipart/form-data>
+         <input type=file name=file>
+         <input type=submit value=上传>
+    </form>
+    '''
+'''
 downloadFilePos=""
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY']="UPLOAD"
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -72,13 +81,33 @@ def download(filename):
             # return send_from_directory('upload', filename, as_attachment=True)#original line
 '''
 
-@app.route('/')
-def index():
-    return 'Index Page'
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename) #use secure_filename function for safety
+            photoGallary.append(filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            # new_audio_name = clean_audio(app.config['UPLOAD_FOLDER'] + "/" + filename, "em_lyrics.txt")
+            # return redirect(url_for('uploaded_file',filename=filename))
+            return redirect(request.url)
+    return html
 
 if __name__ == "__main__":
     app.run()
