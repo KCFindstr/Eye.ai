e316b0b540a9aa3a48077f2384d9f2147be5a987
Yuyang Huang <yuyangh@usc.edu>
2018-03-31 22:35:10 -0700

    recover init.py

--- html/eyeai/__init__.py
+++ html/eyeai/__init__.py
@@ -3,18 +3,22 @@ from flask import Flask, request, redirect, url_for, flash, send_file, render_te
 from werkzeug.utils import secure_filename
 import requests
 import json
+from PIL import Image
+from detection_inference_api import object_detection
+from config import *
 
 app = Flask(__name__)
 
 # allowed format
-ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'pdf'])
-UPLOAD_FOLDER = '/Users/yuyang/PycharmProjects/eye.ai/test'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
+# UPLOAD_FOLDER set in the config.py
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = "UPLOAD"
 # introduction http://flask.pocoo.org/docs/0.12/quickstart/#static-files
 # url_for('static', filename='style.css')
 
 # used to store pictures's name
+# only use when save function is available
 photoGallary = []
 UPPER_SIZE_photoGallary = 20;
 
@@ -31,84 +35,93 @@ html = '''
 
 
 def allowed_file(filename):
-	return '.' in filename and \
-		   filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/home', methods=['PUT', 'POST'])
 def upload_file():
-	if request.method == 'POST':
-		# check if the post request has the file part
-		# if 'file' not in request.files:
-		#     flash('No file part')
-		#     return redirect(request.url)
-		file = request.files['image']
-		# if user does not select file, browser also
-		# submit a empty part without filename
-		if file.filename == '':
-			flash('No selected file')
-			return redirect(request.url)
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)  # use secure_filename function for safety
-			# update the file & clean the photo cache
-			cleanPhotoCache()
-			print(photoGallary)
-			print(request.form)
-			# revise the filename with order
-			filename = filename.rsplit('.', 1)[0].lower() + str(len(photoGallary)) + "." + \
-					   filename.rsplit('.', 1)[1].lower()
-			photoGallary.append(filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			return redirect(request.url)
+    if request.method == 'POST':
+        # check if the post request has the file part
+        # if 'file' not in request.files:
+        #     flash('No file part')
+        #     return redirect(request.url)
+        file = request.files['image']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)  # use secure_filename function for safety
+            # update the file & clean the photo cache
+            # cleanPhotoCache()
+
+            # revise the filename with order
+            filename = filename.rsplit('.', 1)[0].lower() + str(len(photoGallary)) + "." + \
+                       filename.rsplit('.', 1)[1].lower()
+
+            photoGallary.append(filename)
+            # save file
+            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            image = Image.open("test/" + filename)
+            # send this image object to zhou jincheng
+
+            return redirect(request.url)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-	'''
-	#first render the template, then send post
-	return redirect(url_for('upload_file'))
-	'''
-	cleanPhotoCache()
-	if request.method == 'POST':
-		print(request.form)
-		#  upload the photo
-		upload_file()
-		if (request.form['tracking'] == "true"):
-			print("get tracking request")
-			# TODO return json package
-			return "Hello"
-		else:
-			# it is the request for detecting
-			print("get detecting request")
-		print(url_for('upload_file'))
-	return render_template('Launch.html')
+    '''
+    #first render the template, then send post
+    return redirect(url_for('upload_file'))
+    '''
+
+    #cleanPhotoCache()
+    if request.method == 'POST':
+        #  upload the photo
+        upload_file()
+        if (request.form['tracking'] == "true"):
+            print("get tracking request")
+            # TODO return json package
+            return "Hello"
+        else:
+            # it is the request for detecting
+            print("get detecting request")
+
+    return render_template('Launch.html')
 
 
 def situationAnalysis():
-	data = {"spam": "foo", "parrot": 42}
-	#           name,   x-pos, y-pos, area , motion
-	situation = [["Lion", 100, 100.5, 2500, "to the right"],
-				 ["Tiger", 200, 100.5, 2500, "to the right"],
-				 ["UFO", 50, 100.5, 2500, "to the left"]]
-	in_json = json.dumps(situation)  # Encode the data
-	return in_json
+    # array of dictionary
+    situation = [
+        {"name": "Lion", "position": (0.25, 0.5), "size": (0.025, 0.025),
+         "vector": (1, 0), "dangerLevel": 0.7},
+        {"name": "Tiger", "position": (0.56, 0.5), "size": (0.015, 0.035),
+         "vector": (-1, 0), "dangerLevel": 0.8},
+        {"name": "UFO", "position": (0.77, 0.57), "size": (0.005, 0.45),
+         "vector": (0.6, 0.8), "dangerLevel": 0.9}
+    ]
+    in_json = json.dumps(situation)  # Encode the data
+    return in_json
 
-
+'''
 def cleanPhotoCache():
-	# get all files in the upload folder
-	photoGallary = os.listdir(app.config['UPLOAD_FOLDER'])
-	# if there are files not belong to the folder, ignore them
-	for files in photoGallary:
-		if not allowed_file(files):
-			photoGallary.remove(files)
-	# delete files if more than limit
-	while len(photoGallary) > UPPER_SIZE_photoGallary:
-		os.remove(os.path.join(app.config['UPLOAD_FOLDER'], photoGallary[0]))
-		del photoGallary[0]
-
+    # get all files in the upload folder
+    photoGallary = os.listdir(app.config['UPLOAD_FOLDER'])
+    # if there are files not belong to the folder, ignore them
+    for files in photoGallary:
+        if not allowed_file(files):
+            photoGallary.remove(files)
+    # delete files if more than limit
+    while len(photoGallary) > UPPER_SIZE_photoGallary:
+        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], photoGallary[0]))
+        del photoGallary[0]
+'''
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
 
 '''
 # original method, used for storage
